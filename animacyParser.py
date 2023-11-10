879a68980d3d86c641877c4cc9a80810ab7d4aa0
Marie Grace <32784228+mgrace31@users.noreply.github.com>
2023-11-10 08:40:46 -0700

    Create animacyParser.py

---
+++ animacyParser.py
@@ -0,0 +1,11 @@
+# Use a pipeline as a high-level helper
+from transformers import pipeline
+
+pipe = pipeline("token-classification", model="andrewt-cam/bert-finetuned-animacy")
+
+def parse_for_animacy(input_sent):
+    animacy_info = pipe(input_sent)
+    print(animacy_info)
+    return
+
+parse_for_animacy("My name is Clara and I live in Berkeley, California.")

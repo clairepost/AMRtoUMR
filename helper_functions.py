9dd8950984f7a1fdf299525861faecd8078d995e
Claire Benet Post <cpost@usc.edu>
2023-12-10 18:06:51 -0700

    changes to helper function

--- helper_functions.py
+++ helper_functions.py
@@ -34,19 +34,25 @@ def read_training_data(folder):
 
 
        
-    amr_roles= {":mod",
+    amr_roles= {
+        ":mod",
         ":cause",
         ":part", 
         ":consist-of",
         ":source",
         ":destination",
-        ":condition",
-        ":concession"}
+        ":condition"
+        } # I think remove concession
+    
+    amr_roles_in_tail= {
+        "cause-01"
+    }
         
     splits_data = align_graphs_on_AMR_splits(amr_graphs,umr_graphs,amr_roles)
     splits_data_df = pd.DataFrame(splits_data)
     columns = ["file", "sent", "amr_head_name", "amr_tail_name", "amr_role","umr_head_name","umr_tail_name", "umr_role", "amr_head_id", "umr_head_id", "amr_tail_id", "umr_tail_id"]
     splits_data_df.columns= columns
+    splits_data_df.to_csv('sample_df.csv', index=False)
     return splits_data_df
 
         

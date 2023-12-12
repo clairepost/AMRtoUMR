467f2815df234f2a527654ee61b87f9f49f77b82
Marie Grace <32784228+mgrace31@users.noreply.github.com>
2023-12-12 06:09:32 -0700

    rewriting model arch

--- dpl.py
+++ dpl.py
@@ -60,70 +60,164 @@ def data_programming(X,Y):
     f_l = []
     #Y_rules = detect_split_role(X,Y) just using place holder rules for right now
     for l_x in Y_rules:
-        rule_f = (l_x == Y).astype(int)
+        rule_f = (l_x == Y)
         f_l.append(rule_f)
-    return f_l
+    return torch.tensor(f_l,dtype=float)
     
+
 
 def train_model(X,Y):
-    # Define your virtual evidence model, neural network model, and data
-    class VirtualEvidenceModel(nn.Module):
-        def forward(self, X, Y):
-            # Implement logic for the virtual evidence model
-            #return exp(wv * fv(X, Y ))
-            pass
+    class ProbabilisticLogicModule(nn.Module):
+        def __init__(self, input_size, latent_variable_size):
+            super(ProbabilisticLogicModule, self).__init__()
+            # Initialize custom weights
+            self.weight = nn.Parameter(torch.randn(latent_variable_size, 1))
+            print(self.weight.size())
+            self.F_X_Y = data_programming(X,Y)
+            self.double()
             
-    class NeuralNetworkModel(nn.Module):
+
+        def forward(self, X):
+            # Perform a custom operation using the parameters
+            K = torch.exp(torch.matmul(self.weight.t(),self.F_X_Y))
+
+            # Apply an activation function (e.g., ReLU)
+            output = torch.relu(K)
+
+            return output
+
+    class NeuralNetworkModule(nn.Module):
         def __init__(self, input_size, output_size):
-            super(NeuralNetworkModel, self.__init__())
+            super(NeuralNetworkModule, self).__init__()
+            # Replace with your actual neural network initialization
             self.fc = nn.Linear(input_size, output_size)
-        
-        def forward(self, X, Y):
-            # Implement logic for the neural network model
-            return torch.softmax(self.fc(x),dim = 1)
+            self.double()
 
-    #add BERT embeddings to X
+        def forward(self, X):
+            # Replace with your actual neural network forward pass
+            return self.fc(X)
+
+    class DPLModel(nn.Module):
+        def __init__(self, input_size, latent_variable_size, output_size):
+            super(DPLModel, self).__init__()
+            self.probabilistic_logic = ProbabilisticLogicModule(input_size, latent_variable_size)
+            self.neural_network = NeuralNetworkModule(latent_variable_size, output_size)
+            self.double()
+
+        def forward(self, X):
+            # Forward pass through probabilistic logic
+            latent_variables = self.probabilistic_logic(X)
+
+            # Forward pass through neural network
+            predictions = self.neural_network(latent_variables)
+
+            return predictions
+
     embeddings,N,D= get_embeddings(X)
+    f_x_y = data_programming(X,Y)
+    input_size = len(X)
 
-    # Initialize weights w_v for each v and set up K
-    rules = data_programming(X,Y)
-    num_weights = len(rules)
-    phi_0 = [nn.Parameter(torch.randn(1, requires_grad=True)) for _ in range(num_weights)] #initializing random weights for Virtual Evidence
-    K = [np.exp(w[v] * rules[v]) for v in range(len(rules))]
+    print("input_size", type(X))
+    print("latent_var_size", f_x_y.size())
+    print("output_size", type(Y), len(set(Y)))
+    latent_variable_size = len(f_x_y) #number of rules
+    output_size = len(set(Y))
 
 
-    # Initialize virtual evidence model, neural network model, and parameters
-    virtual_evidence_model = VirtualEvidenceModel()
-    neural_network_model = NeuralNetworkModel(input_size,output_size)
-    optimizer_phi = optim.SGD(virtual_evidence_model.parameters(), lr=0.001)
-    optimizer_psi = optim.SGD(neural_network_model.parameters(), lr=0.001)
+    # Instantiate DPL model
+    dpl_model = DPLModel(input_size, latent_variable_size, output_size)
+
+    # Set up optimizer
+    optimizer = torch.optim.SGD(dpl_model.parameters(), lr=0.01)
+
+    # Number of optimization steps
+    num_steps = 1000
+
+    # Optimization loop
+    for step in range(num_steps):
+        # Forward pass through the DPL model
+        predictions = dpl_model(X)
+
+        # Compute negative log-likelihood (replace with your actual likelihood function)
+        negative_log_likelihood = -compute_likelihood(K, predictions)
+
+        # Zero the gradients
+        optimizer.zero_grad()
+
+        # Backpropagation
+        negative_log_likelihood.backward()
+
+        # Update parameters
+        optimizer.step()
+
+        # Optionally print or log the loss for monitoring
+        if step % 100 == 0:
+            print(f"Step {step}, Loss: {negative_log_likelihood.item()}")
+
+# After the optimization loop, your DPL model parameters are trained
+
+# def train_model(X,Y):
+#     # Define your virtual evidence model, neural network model, and data
+#     class VirtualEvidenceModel(nn.Module):
+#         def forward(self, X, Y):
+#             # Implement logic for the virtual evidence model
+#             #return exp(wv * fv(X, Y ))
+#             pass
+            
+#     class NeuralNetworkModel(nn.Module):
+#         def __init__(self, input_size, output_size):
+#             super(NeuralNetworkModel, self.__init__())
+#             self.fc = nn.Linear(input_size, output_size)
+        
+#         def forward(self, X, Y):
+#             #NN takes in X, the tensor
+#             # Implement logic for the neural network model
+#             return torch.softmax(self.fc(X),dim = 1)
+
+#     #add BERT embeddings to X
+#     embeddings,N,D= get_embeddings(X)
+
+#     input_size = embeddings.size(-1)
+    
+#     # Initialize weights w_v for each v and set up K
+#     rules = data_programming(X,Y)
+#     num_weights = len(rules)
+#     phi_0 = [nn.Parameter(torch.randn(1, requires_grad=True)) for _ in range(num_weights)] #initializing random weights for Virtual Evidence
+#     K = [np.exp(phi_0 * [v] * rules[v]) for v in range(len(rules))]
+
+
+#     # Initialize virtual evidence model, neural network model, and parameters
+#     virtual_evidence_model = VirtualEvidenceModel()
+#     neural_network_model = NeuralNetworkModel(input_size,output_size)
+#     optimizer_phi = optim.SGD(virtual_evidence_model.parameters(), lr=0.001)
+#     optimizer_psi = optim.SGD(neural_network_model.parameters(), lr=0.001)
 
     
-    # Number of iterations
-    T = 100
+#     # Number of iterations
+#     T = 100
 
-    # E-step and M-step
-    for t in range(1, T + 1):
-        # E-step: Compute variational approximation q(Y)
-        q_Y = virtual_evidence_model(X, Y)
+#     # E-step and M-step
+#     for t in range(1, T + 1):
+#         # E-step: Compute variational approximation q(Y)
+#         q_Y = virtual_evidence_model(X, Y)
 
-        # M-step: Update virtual evidence model parameters (Φ) and neural network model parameters (Ψ)
-        optimizer_phi.zero_grad()
-        optimizer_psi.zero_grad()
+#         # M-step: Update virtual evidence model parameters (Φ) and neural network model parameters (Ψ)
+#         optimizer_phi.zero_grad()
+#         optimizer_psi.zero_grad()
 
-        # Compute KL divergence for Φ
-        kl_phi = torch.distributions.kl.kl_divergence(q_Y, virtual_evidence_model(X, Y))
-        kl_phi.backward()
-        optimizer_phi.step()
+#         # Compute KL divergence for Φ
+#         kl_phi = torch.distributions.kl.kl_divergence(q_Y, virtual_evidence_model(X, Y))
+#         kl_phi.backward()
+#         optimizer_phi.step()
 
-        # Compute KL divergence for Ψ
-        kl_psi = torch.distributions.kl.kl_divergence(q_Y, neural_network_model(X, Y))
-        kl_psi.backward()
-        optimizer_psi.step()
+#         # Compute KL divergence for Ψ
+#         kl_psi = torch.distributions.kl.kl_divergence(q_Y, neural_network_model(X, Y))
+#         kl_psi.backward()
+#         optimizer_psi.step()
 
-    # Return the learned neural network model
-    learned_neural_network = neural_network_model
-    return learned_neural_network
+#     # Return the learned neural network model
+#     learned_neural_network = neural_network_model
+#     return learned_neural_network
 
 
 if __name__ == "__main__":

ae3710df0eba0b858116c27bacb727e579e67ff6
Marie Grace <32784228+mgrace31@users.noreply.github.com>
2023-12-12 17:04:46 -0700

    moved extract_data

--- baseline.py
+++ baseline.py
@@ -1,16 +1,51 @@
+
+from helper_functions import extract_data
+from rules import detect_split_role
+import os
+import random
+import pandas as pd
 ##The baeline model
 # this will be a rules-only baseline model, extract the test data (animacy info will already be applied)
-from helper_functions import extract_data
+def calc_role(Y_preds):
+    #given a rule prediction with weights, return a list consisting of the most probable of the rules given
+    Y_prob = []
+    for i in range(len(Y_preds)):
+        labels, values = Y_preds[i]
+        result_label = random.choices(labels, weights=values, k=1)[0]
+        Y_prob.append(result_label)
+    return Y_prob
+
 
 
 def run_baseline():
     #extract test data into X,Y
     #run animacy_parser on X, to get tuples
+    x_file = "x_test.csv"
+    y_file = "y_trues_test.csv"
+    out_file = "baseline.csv"
+    # if os.path.exists(x_file) and os.path.exists(y_file):
+    #     X= pd.read_csv(x_file)
+    #     #X_tuples = list(X.to_records(index=False))
+    #     y_true= pd.read_csv(y_file)  
+        
+   # else: 
     X_tupes, y_true = extract_data(False) #setting this argument equal to False means we are getting test data
-    y_pred = ""
-    y_true = ""
+    X =  pd.DataFrame.from_records(X_tupes, columns = ['sent','ne_info' ,'amr_graph','amr_head_name', 'amr_role', 'amr_tail_name'])
+        #X.to_csv(x_file)
+        #y_true.to_csv(y_file)
     #apply rules to X to get predictions
+    y_pred = detect_split_role(X) #gets roles and weights
+    y_pred = calc_role(y_pred) # just picks most prob role
 
+
+    #output preditcions
+    y_pred= pd.Series(y_pred, name='y_rules')
+    df = pd.concat([y_true, y_pred], axis=1)
+    df.to_csv(out_file)
+
+
+    
+    
     #return predictions
     return y_pred,y_true
 
--- helper_functions.py
+++ helper_functions.py
@@ -5,6 +5,35 @@ import re
 import torch
 from sklearn.preprocessing import LabelEncoder
 from alignment import *
+from sklearn.preprocessing import LabelEncoder
+from animacyParser import parse_animacy_runner
+
+def extract_data(training):
+    #training parameter: boolean- True if you want to extract training, false if you want to extract test
+    #creat X's and Y's
+    #X's should be of the form [(amr_head_name1,amr_role1,amr_tail_name1),(h2,r2,t2)]
+    #Y's will be of the form [umr_role1, umr_role2]
+    if training:
+        df = read_training_data("training_data")
+    else:
+        df = read_test_data()
+    X_columns = ['sent','ne_info' ,'amr_graph','amr_head_name', 'amr_role', 'amr_tail_name']
+    Y_columns = ['umr_role']
+    # Create a new DataFrame X with the selected columns
+    X = df[X_columns].copy()
+    Y = df[Y_columns].copy()
+    # If you want X to be a list of tuples, you can use the to_records() method
+    X_tuples = list(X.to_records(index=False))
+
+    # 1st push - format
+    # input: [sent, G, h, r,t],..., ...] of length n
+
+    # 2nd push - access to cause-01: (h, :arg1-of, cause-01)
+
+    # output: ["cause", "reason", .....] of length n (1 choice)
+    # output: [(["cause", "reason"], [.75,.25]), (["mod"],[1]), ....] - initial approach
+
+    return X_tuples, Y["umr_role"]
 
 def remove_comment_lines(input_string):
     # Use regular expression to remove lines starting with #
@@ -35,8 +64,6 @@ def read_training_data(folder):
         
         for i in umr_graphs_str:
             umr_graphs[file].append(create_graph(i))
-
-
        
     amr_roles= {
        ":mod",
@@ -63,8 +90,8 @@ def read_training_data(folder):
     columns = ["file", "sent_i","sent","ne_info", "amr_graph","amr_head_name", "amr_tail_name", "amr_role","umr_head_name","umr_tail_name", "umr_role", "amr_head_id", "umr_head_id", "amr_tail_id", "umr_tail_id"]
 
     splits_data_df.columns= columns
-    splits_data_df.to_csv('sample_df.csv', index=False)
     return splits_data_df
+    
 
 # def reformat_x(X,bert_embeddings, N,D):
 #     #this function flattens the bert embeddings so that the input vecotr can be the appropriate size
@@ -80,11 +107,90 @@ def read_training_data(folder):
 #     combined_inputs_tensor = torch.cat(combined_inputs, dim=0)
 #     return X, embeddings
         
+def read_test_data():
+    #THIS FUNCTION IS MOSTLY COPIED OVER FROM FINALY_PROJECT.IPYNB
+    # put all files in dicts
+    umr_files = {}
+    amr_files = {}
 
+    umr_path = os.getcwd() + '/UMR-data-english'
+    amr_path = os.getcwd() + '/AMR-data-english'
 
-read_training_data("training_data")
+    for f in os.listdir(umr_path):
+        file1 = open(umr_path + '/' + f, 'r')
+        umr_files[f] = file1.read()
 
-from sklearn.preprocessing import LabelEncoder
+    for f in os.listdir(amr_path):
+        file1 = open(amr_path + '/' + f, 'r')
+        amr_files[f] = file1.read()
+
+    #Do not have all of the UMR annotations for the Putin document, so just grab everything before this sentence id from AMR
+    amr_files["putin_ENG_0152_2000_1208-AMR.txt"] = amr_files["putin_ENG_0152_2000_1208-AMR.txt"].split("::id NW_PRI_ENG_0152_2000_1208.13")[0]
+
+    #create file mappings
+    #Change key-names to the mapping above, allows for easier comparison
+    amr_files[1] = amr_files.pop('lindsay-AMR.txt')
+    amr_files[2] = amr_files.pop('lorpt-024_Phillipines_landslide_AMR.txt')
+    amr_files[3] = amr_files.pop('putin_ENG_0152_2000_1208-AMR.txt')
+    amr_files[4] = amr_files.pop('edmund_pope-AMR.txt')
+    amr_files[5] = amr_files.pop('pear-AMR__of__english-umr-0004.txt')
+
+    umr_files[1] = umr_files.pop('lindsay-umr.txt')
+    umr_files[2] = umr_files.pop('Lorelei_lorpt-024_Philippines_Landslide_2023-release.txt')
+    umr_files[3] = umr_files.pop('lorelei_lorpt-151_putin_2023-release.txt')
+    umr_files[4] = umr_files.pop('lorelei_lorpt-152_edmundpope_2023-release.txt')
+    umr_files[5] = umr_files.pop('Pear_Story_2023-release.txt')
+
+    file_map = {1:"Lindsay",2: "Landslide", 3:"Putin",4:"Edmund Pope", 5:"Pear Story"}
+
+    umr_sents = {}
+    all_sentences = []
+    for f in umr_files:
+        umr_sents[f] = re.findall(r'(?<=sentence level graph:\n)\([^#]*(?=\n\n#)', umr_files[f])
+        all_sentences.extend(umr_sents[f])
+
+    amr_sents = {}
+    for f in amr_files:
+        amr_sents[f] = re.findall(r'(?<=[\n])\([^#]*(?=\n|)', amr_files[f])
+
+    #using the str2graph.create_graph() function
+    umr_graphs = {}
+    for file in umr_sents.keys():
+        umr_graphs[file] = []
+        for sent in umr_sents[file]:
+            umr_graphs[file].append(create_graph(sent))
+
+    amr_graphs = {}
+    for file in amr_sents.keys():
+        amr_graphs[file] = []
+        for sent in amr_sents[file]:
+            amr_graphs[file].append(create_graph(sent))
+
+    amr_roles= {
+       ":mod",
+       ":cause",
+       ":part", 
+       ":consist-of",
+       ":source",
+       ":destination",
+       ":condition",
+       ":ARG1-of"
+        } # I think remove concession
+    
+    amr_roles_in_tail= {
+       ":ARG1-of": "cause-01"
+    }
+    umr_t2r = {
+        "cause-01":[":cause", ":reason",":Cause-of"]
+    }
+    ne_info = parse_animacy_runner(all_sentences)
+    splits_data = align_graphs_on_AMR_splits(all_sentences,ne_info, amr_graphs,umr_graphs,amr_roles, amr_roles_in_tail, umr_t2r)
+    splits_data_df = pd.DataFrame(splits_data)
+
+    columns = ["file", "sent_i","sent","ne_info", "amr_graph","amr_head_name", "amr_tail_name", "amr_role","umr_head_name","umr_tail_name", "umr_role", "amr_head_id", "umr_head_id", "amr_tail_id", "umr_tail_id"]
+
+    splits_data_df.columns= columns
+    return splits_data_df
 
 def map_categorical_to_tensor(series):
     """
